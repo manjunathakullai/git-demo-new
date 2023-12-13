74eef8cfded5bd5b56455e855e04f45fbfc77937
Manjunatha Kullai <manjunatha.10686666@ltimindtree.com>
2023-12-13 11:38:45 +0530

    this is my first commit of git-demo project

---
+++ src/main/java/git/demo/com/App.java
@@ -0,0 +1,22 @@
+package git.demo.com;
+
+public class App {
+	
+	
+	public void addData(String str) {
+		
+		System.out.println("this is git demo app");
+		
+		System.out.println(str);
+	}
+	public static void main(String[] args) {
+		
+		
+		System.out.println("this is an main method");
+		
+		App app=new App();
+		app.addData("this is an git demo of method ");
+	}
+	
+
+}
